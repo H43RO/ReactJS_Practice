eb06a545739e085f9b5606bbae345fd62c9965f1
H43RO <h2is1234@naver.com>
2020-07-19 22:26:58 +0900

    JSX, Component  개념 익히기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Potato from './potato';
 
-ReactDOM.render( <React.StrictMode><App/></React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render( <App />,document.getElementById('root'));
