62de13fc67b4cea4d44f24567290cfc7c525bfc1
H43RO <h2is1234@naver.com>
2020-07-19 22:37:00 +0900

    JSX, Component 개념 익히기 (데이터 전달 등)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Potato from './potato';
+
+function Food(props){
+  // props 및 속성 이름를 통해 상위 컴포넌트에서 전달된 정보를 접근 가능
+  return <h3>I like {props.fav}</h3>
+}
 
 function App() {
     return <div>
              <h1>Hello</h1>
-             <Potato/>
+             <Food fav="Kimchi"/>
+             <Food fav="Pizza"/>
+             <Food fav="Hamburger"/>
+             <Food fav="Ramen"/>
            </div>
     ;
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Potato from './potato';
 
 ReactDOM.render( <App />,document.getElementById('root'));
