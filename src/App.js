3f0c783fd1fc0b93b0e4b142ca7d4bc6e9935ccd
H43RO <h2is1234@naver.com>
2020-07-19 23:31:14 +0900

    Dynamic Component 생성 연습

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React from 'react';
 
 function Food(props){
   // props 및 속성 이름를 통해 상위 컴포넌트에서 전달된 정보를 접근 가능
-  return <h3>I like {props.fav}</h3>
+  return <div>
+    <h3>I like {props.name}</h3>
+    <img src={props.picture}/>
+  </div>
 }
+
+const foodILike = [
+  {
+    name:"Kimchi",
+    image:"https://www.koreanbapsang.com/wp-content/uploads/2016/10/DSC_1843-e1477360668451.jpg"
+  },
+  {
+    name:"Bibimbap",
+    image:"https://minimalistbaker.com/wp-content/uploads/2019/05/Bibimbap-SQUARE-500x500.jpg"
+  },
+  {
+    name:"Pizza",
+    image:"https://www.pizzamarumyanmar.com/wp-content/uploads/2019/04/Bulgogi-pizza-2.jpg"
+  }
+]
 
 function App() {
     return <div>
-             <h1>Hello</h1>
-             <Food fav="Kimchi"/>
-             <Food fav="Pizza"/>
-             <Food fav="Hamburger"/>
-             <Food fav="Ramen"/>
+              {foodILike.map(dish => (
+                <Food name={dish.name} picture={dish.image}/>
+              ))}
            </div>
     ;
 }
