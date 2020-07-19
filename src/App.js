d0b2e2303986421c5adc2bbbb4c8d9ba07368614
H43RO <h2is1234@naver.com>
2020-07-20 00:20:53 +0900

    Prop Types 라이브러리적용

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const foodILike = [
+  {
+    id:1,
+    name:"Kimchi",
+    image:"https://www.koreanbapsang.com/wp-content/uploads/2016/10/DSC_1843-e1477360668451.jpg",
+    rating: 5
+  },
+  {
+    id:2,
+    name:"Bibimbap",
+    image:"https://minimalistbaker.com/wp-content/uploads/2019/05/Bibimbap-SQUARE-500x500.jpg",
+    rating: 4.3
+  },
+  {
+    id:3,
+    name:"Pizza",
+    image:"https://www.pizzamarumyanmar.com/wp-content/uploads/2019/04/Bulgogi-pizza-2.jpg",
+    rating: 3.4
+  }
+]
 
 function Food(props){
   // props 및 속성 이름를 통해 상위 컴포넌트에서 전달된 정보를 접근 가능
   return <div>
-    <h3>I like {props.name}</h3>
-    <img src={props.picture}/>
+    <h2>I like {props.name}</h2>
+    <h4>{props.rating}/5</h4>
+    <img src={props.picture} alt={props.name}/>
   </div>
 }
 
-const foodILike = [
-  {
-    name:"Kimchi",
-    image:"https://www.koreanbapsang.com/wp-content/uploads/2016/10/DSC_1843-e1477360668451.jpg"
-  },
-  {
-    name:"Bibimbap",
-    image:"https://minimalistbaker.com/wp-content/uploads/2019/05/Bibimbap-SQUARE-500x500.jpg"
-  },
-  {
-    name:"Pizza",
-    image:"https://www.pizzamarumyanmar.com/wp-content/uploads/2019/04/Bulgogi-pizza-2.jpg"
-  }
-]
+//인식을 위해 꼭 propTypes라고 이름을 지어야함
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
 
 function App() {
     return <div>
-              {foodILike.map(dish => (
-                <Food name={dish.name} picture={dish.image}/>
-              ))}
+              {foodILike.map(dish=>
+                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+                )}
            </div>
     ;
 }
