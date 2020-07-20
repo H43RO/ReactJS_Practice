b6d4104506d480c43ea26b010e451c167e3a82a9
H43RO <h2is1234@naver.com>
2020-07-20 15:23:52 +0900

    Class Component + State 개념

--- src/App.js
+++ src/App.js
@@ -1,50 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id:1,
-    name:"Kimchi",
-    image:"https://www.koreanbapsang.com/wp-content/uploads/2016/10/DSC_1843-e1477360668451.jpg",
-    rating: 5
-  },
-  {
-    id:2,
-    name:"Bibimbap",
-    image:"https://minimalistbaker.com/wp-content/uploads/2019/05/Bibimbap-SQUARE-500x500.jpg",
-    rating: 4.3
-  },
-  {
-    id:3,
-    name:"Pizza",
-    image:"https://www.pizzamarumyanmar.com/wp-content/uploads/2019/04/Bulgogi-pizza-2.jpg",
-    rating: 3.4
-  }
-]
+//Class Component
+class App extends React.Component{
+  //동적으로 변하는 데이터가 들어가는 'State'
+  //state의 데이터가 변하면 render 재호출 (with virtual DOM)
+  state = {
+    count: 0
+  };
 
-function Food(props){
-  // props 및 속성 이름를 통해 상위 컴포넌트에서 전달된 정보를 접근 가능
-  return <div>
-    <h2>I like {props.name}</h2>
-    <h4>{props.rating}/5</h4>
-    <img src={props.picture} alt={props.name}/>
-  </div>
-}
+  add = () => {
+    this.setState( current => ({count: current.count + 1}) );
+  };
 
-//인식을 위해 꼭 propTypes라고 이름을 지어야함
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
+  minus = () => {
+    this.setState( current => ({ count: current.count - 1}) );
+  };
 
-function App() {
+  //화면에 표시할 부분을 render method에 넣어야함 (React가 화면 구성시 자동으로 실행)
+  render(){
     return <div>
-              {foodILike.map(dish=>
-                <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-                )}
+              <h1>The number is : {this.state.count}</h1>
+              <button onClick={this.add}>Add</button>
+              <button onClick={this.minus}>Minus</button>
            </div>
-    ;
+  }
 }
 
 export default App;
